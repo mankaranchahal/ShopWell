f7c6a4ef2ad3f57888353d1086eef9e4b949a0d8
Mankaran Chahal <Mankaran_Chahal@epam.com>
2017-01-14 21:49:13 +0530

    commit after changes in  app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // connect to MongoDB
-mongoose.connect('mongodb://localhost/shopping')
+mongoose.connect('mongodb://userUM2:rMJh0k5qNDFRLbtK@mongodb/sampledb')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
